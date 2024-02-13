3aa0cdb2498aadf6cab6c839461e0c9db42257e1
m.alramaly <96423919+alramaly22@users.noreply.github.com>
2024-02-13 18:57:56 +0200

    Add files via upload

---
+++ js/language.js
@@ -0,0 +1,238 @@
+let lang = {
+    ar: {
+        navhome: "الرئيسية",
+        navabout: "حول",
+        navpricing: "التسعير",
+        navsteps:"خطوات",
+        navfaqs:"الأسئلة الشائعة",
+
+
+        homespan:"استعد للتحول",
+        homespan2:"ابدأ رحلتك في اللياقة البدنية",
+        homeh3:"الرحلة الآن",
+        
+        aboutspan1:"مزيد من المعلومات",
+        aboutspan2:"عن",
+        aboutspan3:"الروخ",
+        aboutdiscription:"الكابتن محمد الرخ هو مدرب لياقة معتمد متعاقد مع الاتحادات المصرية والأفريقية لرفع الأثقال. بفضل خبرته، يقدم برامج تدريب فعالة مصممة خصيصًا لتلبية احتياجات كل فرد، سواء كان ذلك شخصيًا أو عبر الإنترنت. يشتهر بتفانيه في تحفيز العملاء لتحقيق أهدافهم الرياضية. كما يمتلك 'Muscles State EG' صالات رياضية، والتي توفر بيئة ملائمة للتمارين الرياضية الفعّالة.",
+
+        transformation_title:"التحولات",
+
+        stepsh2span1:"احتفل بلياقتك",
+        stepsh2span2:"الرحلة: الخطوات الأولى",
+        stepsh31:"ابدأ الآن",
+        stepsp1:"ابدأ رحلتك نحو الجسم المثالي بالنقر على زر 'ابدأ الآن'",
+        stepsh32:"سجل النموذج",
+        stepsp2:"الآن، أجب على جميع الأسئلة في النموذج وانقر على زر 'إرسال'",
+        stepsh33:"التواصل",
+        stepsp3:"سيقوم المدرب الرياضي محمد الرخ بمراجعة جميع بياناتك الشخصية والتواصل معك عبر تطبيق ال WhatsApp. سيقوم بإرسال برنامج تدريب مخصص سيساعد في تحقيق التحول المطلوب لجسمك في وقت قصير.",
+
+        pricingh2:"خطة التسعير الخاصة بنا",
+        pricingh41:"الخطة العادية",
+        pricingh311:"شهرياً",
+        pricingh312:" جنيه 1000",
+        pricingspan11:"خطة تغذية أسبوعية مخصصة استنادًا إلى أطعمتك المفضلة",
+        pricingspan12:"برنامج تدريب مصمم للمنزل أو الصالة الرياضية، مع مقاطع فيديو توجيهية",
+        pricingspan13:"تواصل مباشر عبر WhatsApp ومتابعة",
+        pricingbutton:"انضم الآن",
+        pricingh321:"أشهر3",
+        pricingh322:"2500 جنيه",
+        pricingh42:"الاستعداد للبطولات",
+        pricingh332:"2000 جنيه",
+        pricingspan16:"الاستعداد للبطولات القادمة",
+        pricingspan15:"برنامج تدريب مكثف مركز على جاهزية التنافس",
+        pricingspan14:"تخطيط تغذوي متقدم مصمم لتحقيق الأداء القصوى",
+        pricingh342:"5000 جنيه",
+
+
+        experienceh2:"انضم إلى مجموعة من الأبطال",
+        experiencespan11:"سنة",
+        experiencespan12:"خبراه",
+        experiencespan21:"عملاء",
+        experiencespan31:"التحولات",
+        experiencespan32:"الناجحة",
+        experiencebutton:"ابدأ الآن",
+
+
+        questionsh1:"الأسئلة الشائعة",
+        questionsq1:"لماذا يعتبر التدريب مع المدرب محمد الرخ أساسيًا؟",
+        questionsa1:"يعتبر التدريب مع المدرب محمد الرخ أمرًا حيويًا لأنه يتمتع بثروة من الخبرة والاختصاص في تحقيق أهداف اللياقة البدنية. بفضل الشهادات من الاتحادات المصرية والأفريقية لرفع الأثقال، يعرف المدرب محمد بالضبط كيف يُعد تمارين يمكن تصميمها حسب الاحتياجات لتحقيق النتائج المثلى لعملائه",
+        questionsq2:"كيف يمكن للمدرب محمد الرخ تحويل جسمك؟",
+        questionsa2:"تمكنه الخبرة الواسعة والتفاني من تصميم برامج تدريبية مخصصة تستهدف بفعالية أهداف اللياقة البدنية الخاصة. سواء كنت تهدف إلى فقدان الوزن، أو بناء العضلات، أو تحسين الصحة العامة، يقدم المدرب محمد الدعم والتوجيه اللازمين لتحقيق الجسم المثالي",
+        questionsq3:"لماذا تختار المدرب محمد الرخ لرحلتك في اللياقة البدنية؟",
+        questionsa3:"سجل المدرب محمد الرخ الناجح وتفانيه في خدمة عملائه يجعله الخيار المثالي لأي شخص يسعى لتحويل جسمه. بصفته مالكًا لصالات اللياقة البدنية 'Muscles State EG' ومدرب لياقة معتمد، يضمن أن يتلقى كل عميل اهتمامًا شخصيًا ويحقق نتائج استثنائية",
+        questionsq4:"ما الذي يميز تدريب المدرب محمد الرخ؟",
+        questionsa4:"يميز تدريب المدرب محمد الرخ ثراء الخبرة والخبرة الواسعة في تحقيق أهداف اللياقة البدنية، بالإضافة إلى حصوله على شهادات من الاتحادين المصري والأفريقي لرفع الأثقال، مما يمكنه من تصميم تمارين مخصصة لتحقيق أقصى نتائج لعملائه.",
+        questionsq5:"ما الفوائد التي يمكن أن يجلبها تدريب محمد الرخ لعملائه؟",
+        questionsa5:" يمكن لتدريب محمد الرخ أن يجلب العديد من الفوائد، بما في ذلك الحصول على برامج تدريبية مخصصة وفقًا لاحتياجات العملاء، والتوجيه والدعم المستمر لتحقيق أهداف اللياقة البدنية، بالإضافة إلى تجربة واسعة في مجال تحقيق النتائج الملموسة والملموسة في اللياقة البدنية",
+        questionsq6:"كيف يمكن أن يساعد الكابتن محمد الرخ العملاء خارج الحصص التدريبية؟",
+        questionsa6:"يُعتبر الكابتن محمد الرخ أن عملاءه أصدقاء، وبالتالي فهو ملتزم بتقديم الدعم والمشورة لهم لتحسين نوعية حياتهم بشكل عام، سواء كان ذلك من خلال توجيهات تغذية متوازنة، أو نصائح للحفاظ على التوازن بين العمل والحياة الشخصية، أو حتى تقديم الدعم العاطفي والمعنوي لهم في تحقيق أهدافهم وتجاوز التحديات",
+
+        calculateh1:"احسب مؤشر كتلة الجسم الخاص بك",
+        calculatep:"مُحسِّب مؤشر كتلة الجسم (BMI) يقوم بحساب مؤشر كتلة الجسم من وزنك وطولك",
+        calculatebutton:"احسب الآن",
+
+
+        footerp:"الكابتن محمد الرخ: مدرب لياقة معتمد. مشهور بالتدريب المخصص. مالك صالات 'Muscles State EG'. خبير في التغذية والتمارين. ملتزم بأهداف العملاء",
+        footerh31:"حول",
+        footera1:"حولنا",
+        footera2:"الأسئلة الشائعة",
+        footera3:"الخبرة",
+        footerh32:"الروابط",
+        footera4:"التحول",
+        footera5:"الخطوات",
+        footera6:"الأسعار",
+        footerh33:"الهدية",
+        footera7:"الصفحة الرئيسية",
+        footera8:"احسب",
+    },
+    en: {
+        navhome:"Home",
+        navabout:"About",
+        navpricing:"Pricing",
+        navsteps:"Steps",
+        navfaqs:"FAQs",
+
+
+        homespan:"Get Ready for Transformation",
+        homespan2:"Start Your Fitness",
+        homeh3:"Start Now",
+        
+        aboutspan1:"More Information",
+        aboutspan2:"About",
+        aboutspan3:"El Rokh",
+        aboutdiscription:"Captain Mohamed El Rokh is a certified fitness trainer affiliated with the Egyptian and African Bodybuilding Federations. With his expertise, he delivers effective training programs tailored to individual needs, both in-person and online. Known for his dedication, he motivates clients to achieve their fitness goals. He also owns 'Muscles State EG' gyms, providing a conducive environment for effective workouts",
+
+        transformation_title:" Transformation",
+
+        stepsh2span1:"Celebrate Your Fitness",
+        stepsh2span2:"Journey  The Starting Steps",
+        stepsh31:"Get Started",
+        stepsp1:"Encourage your clients to take the first step by clicking on the 'Start Now' button",
+        stepsh32:"Register the Form",
+        stepsp2:"In the next step, your clients will need to answer all questions in the form and then click the 'Submit' button",
+        stepsh33:"Connect",
+        stepsp3:"Coach El Rokh will review all your personal data and reach out to you via WhatsApp. He'll send you a customized training program that will transform your body into perfection in no time",
+
+        pricingh2:"Our Pricing Plan",
+        pricingh41:"Normal Plan",
+        pricingh311:"Monthly",
+        pricingh312:" $50",
+        pricingspan11:"Weekly customized meal plan based on your favorite foods",
+        pricingspan12:"Training regimen tailored for home or gym, with instructional videos",
+        pricingspan13:"Direct WhatsApp communication and follow-up",
+        pricingbutton:"Join Now",
+        pricingh321:"3 Months",
+        pricingh322:" $100",
+        pricingh42:"Tournaments Preparation",
+        pricingh332:" $100",
+        pricingspan16:"Preparation for upcoming tournaments",
+        pricingspan15:"Intensive training regimen focused on competition readiness",
+        pricingspan14:"Advanced nutritional planning tailored to peak performance",
+        pricingh342:" $250",
+
+
+        experienceh2:"Join a group of heroes",
+        experiencespan11:"Year ",
+        experiencespan12:" Experience",
+        experiencespan21:"Clients",
+        experiencespan31:"Successful",
+        experiencespan32:"transformations",
+        experiencebutton:"Start Now ",
+
+
+        questionsh1:" Common Questions",
+        questionsq1:"Why is training with Coach Mohamed El Rokh essential?",
+        questionsa1:"Training with Coach Mohamed El Rokh is crucial because he brings a wealth of experience and expertise in achieving fitness goals. With certifications from both the Egyptian and African Bodybuilding Federations, Coach Mohamed knows precisely how to tailor workouts to maximize results for his clients",
+        questionsq2:"How can Coach Mohamed El Rokh transform your body?",
+        questionsa2:"Coach Mohamed El Rokh's extensive experience and dedication enable him to design personalized training programs that effectively target specific fitness goals. Whether you aim to lose weight, build muscle, or improve overall health, Coach Mohamed provides the guidance and support needed to achieve the ideal physique",
+        questionsq3:"Why choose Coach Mohamed El Rokh for your fitness journey?",
+        questionsa3:"Coach Mohamed El Rokh's track record of success and his dedication to his clients make him the perfect choice for anyone looking to transform their body. As the owner of 'Muscles State EG' gyms and a certified fitness trainer, he ensures that each client receives personalized attention and achieves exceptional results",
+        questionsq4:"Why is training with Coach Mohamed El Rokh essential?",
+        questionsa4:"Training with Coach Mohamed El Rokh is essential because he brings a wealth of experience and expertise in achieving fitness goals. With certifications from both the Egyptian and African Bodybuilding Federations, Coach Mohamed knows precisely how to tailor workouts to maximize results for his clients",
+        questionsq5:"How can Coach Mohamed El Rokh transform your body?",
+        questionsa5:"Coach Mohamed El Rokh can transform your body through his extensive experience and dedication. He designs personalized training programs that effectively target specific fitness goals, whether you aim to lose weight, build muscle, or improve overall health. With his guidance and support, you can achieve the ideal physique",
+        questionsq6:"Why choose Coach Mohamed El Rokh for your fitness journey?",
+        questionsa6:"There are several reasons to choose Coach Mohamed El Rokh for your fitness journey. Firstly, he has a proven track record of success and dedication to his clients. Additionally, as the owner of 'Muscles State EG' gyms and a certified fitness trainer, he ensures that each client receives personalized attention and achieves exceptional results",
+
+        calculateh1:"Calculate Your Bmi",
+        calculatep:"The body mass index (BMi) calculator calculates body mass index from your weight and height ",
+        calculatebutton:"Calculate Now",
+
+
+        footerp:"Captain Mohamed El Rokh: Certified fitness coach. Renowned for personalized  training. Owner of ' uscles State EG ' gyms Expert in nutrition and exercise Dedicated to clients' goals",
+        footerh31:"About",
+        footera1:"About Us",
+        footera2:"FAQs",
+        footera3:"Experience",
+        footerh32:"Links",
+        footera4:"Transformation",
+        footera5:"Steps",
+        footera6:"Pricing",
+        footerh33:"Gift",
+        footera7:"Home",
+        footera8:"Calculate",
+    }
+};
+
+let selector = document.getElementById('language-selector');
+
+// استدعاء الدالة عند تغيير اللغة
+selector.addEventListener('change', updateLanguage);
+
+function updateLanguage() {
+    let language = selector.value; // اللغة المختارة
+    let nodes = document.querySelectorAll('[data-lang]');
+
+    // تحديث نصوص العناصر باستخدام اللغة المناسبة
+    nodes.forEach(node => {
+        let key = node.getAttribute('data-lang');
+        if (lang[language] && lang[language][key]) {
+            node.textContent = lang[language][key];
+        }
+    });
+}
+
+
+// استدعاء الدالة لتعيين اللغة الافتراضية عند تحميل الصفحة
+updateLanguage();
+
+function updateLanguage() {
+    let language = selector.value; // اللغة المختارة
+    let nodes = document.querySelectorAll('[data-lang]');
+
+    // تحديث نصوص العناصر وتبديل الفئات الخاصة بـ font-family
+    nodes.forEach(node => {
+        let key = node.getAttribute('data-lang');
+        if (lang[language] && lang[language][key]) {
+            node.textContent = lang[language][key];
+        }
+
+        if (language === 'ar') {
+            node.classList.remove('english-font');
+            node.classList.add('arabic-font');
+        } else {
+            node.classList.remove('arabic-font');
+            node.classList.add('english-font');
+        }
+
+        // إضافة الفئة الإضافية هنا (مثل 'second-arabic-font') إذا كان ذلك مطلوبًا
+        // وتبديلها بناءً على اللغة المحددة بنفس الطريقة
+        if (language === 'ar') {
+            node.classList.remove('english-font');
+            node.classList.add('second-arabic-font');
+        } else {
+            node.classList.remove('second-arabic-font');
+            node.classList.add('english-font');
+        }
+        if (language === 'ar') {
+            node.classList.remove('english-font');
+            node.classList.add('se-arab');
+        } else {
+            node.classList.remove('se-arab');
+            node.classList.add('english-font');
+        }
+    });
+}
+
+
